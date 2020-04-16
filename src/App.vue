9b1a02541c6b094eb579eafabbcf0093dcd68639
Matt Crandell <mrcrandell@gmail.com>
2020-04-15 21:51:14 -0400

    Removed fake nav.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!--<div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/style-guide">Style Guide</router-link>
-    </div>
+    </div>-->
     <SiteHeader></SiteHeader>
     <SiteNav></SiteNav>
     <router-view/>
