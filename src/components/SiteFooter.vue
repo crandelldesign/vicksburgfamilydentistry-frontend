d11123316b49503d110101b6e36ff3c77f3af3d8
Matt Crandell <mrcrandell@gmail.com>
2021-01-04 22:41:12 -0500

    Updated http possible errors.

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,8 +1,22 @@
 <template>
   <footer class="footer">
-    <p>602 West Prairie Street, Vicksburg, MI 49097 &middot; <a href="tel:12696491495">269-649-1495</a></p>
-    <p>Copyright &copy; {{ new Date().getFullYear() }} Vicksburg Family Dentistry. All Rights Reserved.</p>
-    <p>Designated trademarks and brands are the property of their respective owners.</p>
-    <p>Website created by Matt Crandell of <a href="http://www.crandelldesign.com" target="_blank">Crandell Design</a>.</p>
+    <p>
+      602 West Prairie Street, Vicksburg, MI 49097 &middot;
+      <a href="tel:12696491495">269-649-1495</a>
+    </p>
+    <p>
+      Copyright &copy; {{ new Date().getFullYear() }} Vicksburg Family
+      Dentistry. All Rights Reserved.
+    </p>
+    <p>
+      Designated trademarks and brands are the property of their respective
+      owners.
+    </p>
+    <p>
+      Website created by Matt Crandell of
+      <a href="https://www.crandelldesign.com" target="_blank"
+        >Crandell Design</a
+      >.
+    </p>
   </footer>
 </template>
