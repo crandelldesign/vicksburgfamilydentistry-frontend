bfaaa9b812378b20eb32b18a91f72c0e89d73592
Matt Crandell <mrcrandell@gmail.com>
2022-07-05 21:20:21 -0400

    updated about us.

--- src/components/SectionAboutUs.vue
+++ src/components/SectionAboutUs.vue
@@ -1,16 +1,49 @@
 <template>
   <section class="section about-us" id="about-us">
-      <div class="content">
-          <h2>About Us</h2>
-          <h3>Dr. Aaron C. Ford</h3>
-          <p>Dr. Ford grew up in Sturgis, Michigan and earned his bachelors degree from Western Michigan University in 1999.  While at Western, he was involved in the Army ROTC program.  After graduation, Dr. Ford was commissioned in the United States Army as a Second Lieutenant.  He served two years at Fort Hood, Texas in the 1st Calvary Division where he was a medical platoon leader in the Medical Service Corps.  His next assignment took him to Walter Reed Army Medical Center in Washington, DC, where he spent the next two years in the Health Physics Department.</p>
-          <p>After his commitment with the Army, Dr. Ford entered dental school in 2003 at the University of Michigan and graduated in 2007.  He moved to Vicksburg in 2009 and really enjoys living in south west Michigan.  Dr. Ford is married to Maureen McGlinchey Ford and they have three children.</p>
-          <p>He is a member of the American Dental Association, Michigan Dental Association, and Kalamazoo Valley District Dental Society.  He participates in many continuing education courses throughout the year.</p>
+    <div class="content">
+      <h2>About Us</h2>
+      <h3>Dr. Aaron C. Ford</h3>
+      <p>
+        Dr. Ford grew up in Sturgis, Michigan and earned his bachelors degree
+        from Western Michigan University in 1999. While at Western, he was
+        involved in the Army ROTC program. After graduation, Dr. Ford was
+        commissioned in the United States Army as a Second Lieutenant. He served
+        two years at Fort Hood, Texas in the 1st Calvary Division where he was a
+        medical platoon leader in the Medical Service Corps. His next assignment
+        took him to Walter Reed Army Medical Center in Washington, DC, where he
+        spent the next two years in the Health Physics Department.
+      </p>
+      <p>
+        After his commitment with the Army, Dr. Ford entered dental school in
+        2003 at the University of Michigan and graduated in 2007. He moved to
+        Vicksburg in 2009 and really enjoys living in south west Michigan. Dr.
+        Ford is married to Maureen McGlinchey Ford and they have three children.
+      </p>
+      <p>
+        He is a member of the American Dental Association, Michigan Dental
+        Association, and Kalamazoo Valley District Dental Society. He
+        participates in many continuing education courses throughout the year.
+      </p>
 
-          <h3>Office Staff</h3>
-          <p>The Office is managed by Kris McGlothlen who brings in 21 years of dental experience. The front desk is run by Jessica and Cheryl, who will greet with a smile and are very willing to help you with your insurance and billing questions.</p>
-          <p>Paula and Holly are Dr Ford's assistants. They are very good at making you feel comfortable during procedures and always have a warm friendly smile.</p>
-          <p>Heidi, Colleen, Dana, Jen, Kelly and Jamie are our hygienists.  They all do an excellent job cleaning your teeth while keeping you comfortable. They have a wealth of knowledge concerning your oral hygiene needs.  Be sure to ask them anything that may be on your mind concerning your oral hygiene.</p>
-      </div>
+      <h3>Office Staff</h3>
+      <p>
+        The Office is managed by Kris McGlothlen who brings in 23 years of
+        dental experience. The front desk is run by Jessica and Cheryl, who will
+        greet with a smile and are very willing to help you with your insurance
+        and billing questions.
+      </p>
+      <p>
+        Paula and Gaye are Dr Ford's assistants. They are very good at making
+        you feel comfortable during procedures and always have a warm friendly
+        smile.
+      </p>
+      <p>
+        Heidi, Dana, Jamie, Kelly, Tara and Alex are our hygienists. They all do
+        an excellent job cleaning your teeth while keeping you comfortable. They
+        have a wealth of knowledge concerning your oral hygiene needs. Be sure
+        to ask them anything that may be on your mind concerning your oral
+        hygiene.
+      </p>
+    </div>
   </section>
 </template>
